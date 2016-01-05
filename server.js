497eb495bc7e8799bcf543dcd8e7e8c9fd3c9bba
ammark47 <ammark1111@gmail.com>
2016-01-05 14:20:48 -0500

    trying to add static files

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var helmet = require('helmet');
 var app = express();
 
 app.use(helmet());
-var staticPath = path.resolve(__dirname, '/build');
+var staticPath = path.resolve(__dirname, '../build');
 app.use(express.static(staticPath));
 
 var server_port = process.env.PORT || CONFIG.port;
@@ -12,6 +12,7 @@ var server_port = process.env.PORT || CONFIG.port;
  
 app.listen(server_port, function () {
   console.log( "Listening on server_port " + server_port );
+  console.log(__dirname); 
 });
 
 app.get("/", (req, res) => {
