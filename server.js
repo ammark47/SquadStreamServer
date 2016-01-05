aca332d3c1d95af070c108a17e4953f4ed712119
ammark47 <ammark1111@gmail.com>
2016-01-05 12:32:37 -0500

    update for openshift

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ app.use(helmet());
 var staticPath = path.resolve(__dirname, '/build');
 // app.use(express.static(staticPath));
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var server_port = 8080;
+var server_ip_address = '127.0.0.1';
  
 app.listen(server_port, server_ip_address, function () {
   console.log( "Listening on " + server_ip_address + ", server_port " + server_port );
