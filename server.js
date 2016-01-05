a655746733b6d4852c05ecc6264a8e60e362ebe4
ammark47 <ammark1111@gmail.com>
2016-01-05 10:03:18 -0500

    use proper index file

--- server.js
+++ server.js
@@ -3,11 +3,6 @@ var express = require('express');
 var helmet = require('helmet');
 var app = express();
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 app.use(helmet());
 var staticPath = path.resolve(__dirname, '/build');
 app.use(express.static(staticPath));
@@ -17,6 +12,6 @@ app.get("/", (req, res) => {
  res.sendFile(__dirname + "/build/index.html");
 });
 
-app.listen(3000, function() {
+app.listen(8080, '127.0.0.1', function() {
   console.log('listening');
 });
